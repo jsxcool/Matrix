554f551c00eefe1b2e45a7a924bc414a0d7efe5a
Ninesss <36767216+Ninesss@users.noreply.github.com>
2018-03-26 21:06:55 -0400

    add
    add

---
+++ add.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "Matrix.hh"
+using namespace std;
+
+Matrix::Matrix(int rows, int cols, double i);
+
+Matrix::Matrix operator +(Matrix left, Matrix right) {
+        int i = left.rows;
+        int j = left.cols;
+        int x = right.rows;
+        int y = right.cols;
+        if (i = x && j = y) {
+            Matrix ans;
+            for (int q = 0; q < i; q++) {
+                for (int w = 0; w < j; w++) {
+                    ans.[q][w] = left[q][w] + right[q][w];
+                }
+            }
+        }
+    }
