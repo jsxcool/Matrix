7ead1b092db7a631779b72c3b5532caedd50b187
Yu Xia <yxia15@stevens.edu>
2018-03-26 20:49:41 -0400

    modified

--- Matrix.hh
+++ Matrix.hh
@@ -8,13 +8,13 @@ private:
     double* m;
     
 public:
-    Matrix(int rows, int cols, double i = 0): rowls(rows), cols(cols),m(new double[rows*cols]){
+    Matrix(int rows, int cols, double i = 0): rows(rows), cols(cols),m(new double[rows*cols]){
         for(int j = 0; j < rows * cols; j++)
             m[j] = i;
     }
     
     friend double& operator ()(int i, int j);
-    friend Matrix operator=(Matrix left, Matrix right);
+    Matrix operator=(Matrix left, Matrix right);
     double operator ()(int i, int j) const;
     friend Matrix operator *(Matrix left, Matrix right);
     friend std::ostream& operator << (std::ostream& s, Matrix& m);
